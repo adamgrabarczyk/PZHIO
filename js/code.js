17d7a378db59cc07858ddf39a1471626244f383c
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2017-08-09 09:01:18 +0200

    blah

--- js/code.js
+++ js/code.js
@@ -1,3 +1,14 @@
-/**
- * Created by adam on 31.07.17.
- */
+
+window.onload = function () {
+
+    var dropDownButton = document.getElementsByClassName("dropdown");
+
+    dropDownButton.onclick = function (e) {
+
+        e.preventDefault();
+
+    }
+
+
+
+}
